021c8f888084e9e239a9dc8d9e3d8c0c0a97dcec
ypablo <yfedora@gmail.com>
2020-04-05 11:50:19 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.css"
+import { Link } from "react-router-dom"
+import Dog from "../../images/dog-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <div className="nav-logo"><img src={Dog} alt="" /></div>
+            <ul className="nav-links">
+                <Link to="/" style={{ textDecoration: "none", marginRight: "15px" }}>
+                    <li>Home</li>
+                </Link>
+                <Link to="/about" style={{ textDecoration: "none" }}>
+                    <li>About</li>
+                </Link>
+            </ul>
+        </nav>
+    )
+}
