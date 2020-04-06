fcfa9b86883d651c0fc6642a94564feaefac8e6d
ypablo <yfedora@gmail.com>
2020-04-06 07:45:11 +0100

    Add calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,20 +6,36 @@ export default class Calendar extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            bgColor: "#FFD712",
-            isToggleOn: {},
-            array: ["Holiday", "D", "N", "Project", "Edit", "Sick"],
-            colors: ["#FFD712", "#58ACFA", "#58ACFA", "#58ACFA", "#58ACFA", "#FF4000"],
-            font_colors: ["red", "white", "white", "white", "white", "white"],
-            first_click: true,
-            count: 0
+            today: moment(),
+            dateObject: moment(),
+            showMonthPopup: false,
+            showYearPopup: false
         }
     }
 
+    firstDayOfMonth = () => {
+        let dateObject = this.state.dateObject
+        let firstDay = moment(dateObject)
+            .startOf("month")
+            .format("d");
+        return firstDay - 1;
+    }
 
-
-
-
+    daysInMonth = () => {
+        return this.state.dateObject.daysInMonth()
+    }
+    year = () => {
+        return this.state.dateObject.format("Y")
+    }
+    month = () => {
+        return this.state.dateObject.format("MMM")
+    }
+    currentDate = () => {
+        return this.state.dateObject.get("date")
+    }
+    currentDay = () => {
+        return this.state.dateObject.format("D")
+    }
 
 
     render() {
@@ -29,41 +45,70 @@ export default class Calendar extends Component {
                 dow: 1
                 // Monday is the first day of the week.
             }
-        });
+        })
 
-        const weekdayshort = moment.weekdaysShort(true);
-
+        const weekdayshort = moment.weekdaysShort(true)
         let weekdayshortname = weekdayshort.map(day => {
             return (
                 <th key={day} className="week-day">
                     {day}
                 </th>
-            );
-        });
-
-
-
-        //Getting number of days of month
-        const month = new Date().getMonth() + 1
-        const year = new Date().getFullYear()
-        const numberDaysOfMonth = new Date(year, month, 0).getDate();
-
-        let foo1 = []
-        for (let i = 1; i <= numberDaysOfMonth; i++) {
-            foo1.push(i)
-        }
-
-        const gettingDates = foo1.map((element, index) => {
-            return (
-                <div className="day" key={index}> {element}</div>
             )
         })
+
+        //Create blank cell if new month doesn't start Monday
+        let blanks = [];
+        for (let i = 0; i < this.firstDayOfMonth(); i++) {
+            blanks.push(
+                <td className="day empty">{""}</td>
+            )
+        }
+
+        //Start filling with the first date of the month in a render function
+        let daysInMonth = [];
+        for (let d = 1; d <= this.daysInMonth(); d++) {
+            daysInMonth.push(
+                <td key={d} className="day">
+                    {d}
+                </td>
+            );
+        }
+
+        //totalSlots contains blanks and daysInMonth using the spread operator. 
+        var totalSlots = [...blanks, ...daysInMonth];
+        //rows hold  </td> while going to a new row
+        let rows = [];
+        //cells contain each </td> to assign to each row
+        let cells = [];
+
+        //Loop through totalSlots to get a calendar structure of a week.
+        totalSlots.forEach((row, i) => {
+            if (i % 7 !== 0) {
+                cells.push(row); // if index not equal 7 that means not go to next week
+            } else {
+                rows.push(cells); // when reach next week we contain all td in last week to rows 
+                cells = []; // empty container 
+                cells.push(row); // in current loop we still push current row to new container
+            }
+            if (i === totalSlots.length - 1) { // when end loop we add remain date
+                rows.push(cells);
+            }
+        });
+        //Wrap all rows in a </td>
+        let daysinmonth = rows.map((d, i) => {
+            return <tr>{d}</tr>;
+        });
 
         return (
             <div className="calendar">
                 <h1>Calendar</h1>
-                <div className="weekdays">{weekdayshortname}</div>
-                <div className="dates"> {gettingDates}</div>
+
+                <table className="calendar-day">
+                    <thead>
+                        <tr>{weekdayshortname}</tr>
+                    </thead>
+                    <tbody>{daysinmonth}</tbody>
+                </table>
             </div>
         )
     }
