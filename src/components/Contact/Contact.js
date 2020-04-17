600a0e8022f472c1ae35b94897b3eb605b1fd20d
ypablo <yfedora@gmail.com>
2020-04-17 23:47:06 +0100

    Add form and pics

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,49 +1,163 @@
 import React, { Component } from "react"
 import "./Contact.css"
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react"
+import Justyna from "../../images/justyna.jpg"
 
+
+const formValid = (formErrors) => {
+    let valid = true
+    Object.values(formErrors).forEach(val => val.length > 0 && (valid = false))
+    return valid
+}
 
 export class Contact extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            stores: [{ lat: 47.49855629475769, lng: -122.14184416996333 },
-            { latitude: 47.359423, longitude: -122.021071 }]
+            name: '',
+            email: '',
+            comments: '',
+            formErrors: {
+                name: '',
+                email: '',
+                comments: ''
+            }
         }
     }
 
-    /*
-    displayMarkers = () => {
-        return this.state.stores.map((store, index) => {
-            return <Marker key={index} id={index} position={{
-                lat: store.latitude,
-                lng: store.longitude
-            }}
-                onClick={() => console.log("You clicked me!")} />
+    handleUserName = (e) => {
+        /*let formErrors = this.state.formErrors
+        formErrors = value.length < 3 && value.length > 0 ?
+            'Minimum 3 charachters required' :
+            ''
+            */
+        this.setState({
+            name: e.target.value
         })
-    }*/
+    }
+    handleUserEmail = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handleUserComments = (e) => {
+        this.setState({
+            comments: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        if (formValid(this.state.formErrors)) {
+            alert(`Hi ${this.state.name} your message has been submited :D`)
+            console.log(`Submitted: 
+            ${this.state.name} 
+            ${this.state.email} 
+            ${this.state.comments}`)
+        } else {
+            console.error('Form invalid - display error message')
+        }
+    }
+
+
     render() {
-        const mapStyles = {
-            display: "flex",
-            justifyContent: "centre",
-            alignItems: "centre",
-            width: "500px",
-            height: "500px",
-            margin: "25px",
-            border: "1px solid #000"
+        /*const contactPage = {
+            display: 'grid',
+            gridTemplateColumns: "'10%' minmax(300px, 1fr))' 'minmax(300px, 1fr))' '10%'",
+            gridTemplateRows: "'1fr' '1fr'",
+            gridTemplateAreas: "'. pics form .' '. address map .'",
+            justifyItems: 'center',
+            alignItems: 'center',
+            columnGap: '15px',
+            backgroundColor: 'skyblue',
+            height: '100%',
+            margin: '0'
+        }
+        const contactPics = {
+            width: '100%',
+            height: '600px',
+            margin: '10px',
+            gridArea: 'pics'
+        }
+        const contactForm = {
+            width: '100%',
+            height: '600px',
+            margin: '10px',
+            border: '1px solid #000',
+            gridArea: 'form',
+            backgroundColor: '#FFBACD'
+        }*/
+        const contactAddress = {
+            width: '100%',
+            height: '600px',
+            margin: '10px',
+            border: '1px solid #000',
+            gridArea: 'address',
+            backgroundColor: '#4682B4'
+        }
+        const contactMapWrapper = {
+            width: '602px',
+            height: '602px',
+            margin: '10px',
+            border: '1px solid #000',
+            gridArea: 'map'
+        }
+        const contactMap = {
+            width: '600px',
+            height: '600px'
+        }
+        const pic = {
+            width: '100%',
+            height: '100%'
         }
 
 
-
         return (
-            <div className="contact-page" >
-                <Map
-                    google={this.props.google}
-                    style={mapStyles}
-                    zoom={10}
-                    initialCenter={{ lat: 55.9533, lng: -3.1883 }}>
-                    <Marker position={{ lat: 55.9533, lng: -3.1883 }} />
-                </Map>
+            <div /*style={contactPage}*/ className="contact-page" >
+                <div className="wrapper-top">
+                    <div /*style={contactPics}*/ className="contact-pics">
+                        <img src={Justyna} alt="pic1" style={pic} />
+                    </div>
+                    <form /*style={contactForm}*/ className="contact-form" onSubmit={this.handleSubmit}>
+                        <h1>Write something to me !</h1>
+                        <div>
+                            <label>Tell me your name:</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={this.state.name} onChange={this.handleUserName} />
+                        </div>
+                        <div>
+                            <label>And your email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={this.state.email} onChange={this.handleUserEmail} />
+                        </div>
+                        <div>
+                            <label>Your message:</label>
+                            <textarea
+                                value={this.state.comments}
+                                onChange={this.handleUserComments}>
+                            </textarea>
+                        </div>
+                        <button
+                            type="submit" value="Submit">Submit</button>
+                    </form>
+                </div>
+
+                {/*
+                <div style={contactAddress}>
+                    Address + contact + social media
+                </div>
+                <div style={contactMapWrapper}>
+                    <Map
+                        google={this.props.google}
+                        style={contactMap}
+                        zoom={10}
+                        initialCenter={{ lat: 55.9533, lng: -3.1883 }}>
+                        <Marker position={{ lat: 55.9533, lng: -3.1883 }} />
+                    </Map>
+                </div>*/}
             </div >
         )
     }
@@ -52,3 +166,5 @@ export class Contact extends Component {
 export default GoogleApiWrapper({
     apiKey: process.env.REACT_APP_KEY
 })(Contact);
+
+
