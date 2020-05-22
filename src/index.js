aba606e779ee0823d6ee0ed0a7e6fee9ebc1aaa7
ypablo <yfedora@gmail.com>
2020-05-22 02:45:32 +0100

    Add redux

--- src/index.js
+++ src/index.js
@@ -11,10 +11,35 @@ import {Provider} from "react-redux"
 require('dotenv').config()
 
 
+const saveToLocalStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem("state", serializedState)
+    } catch(e) {
+        console.log(e)
+    }
+} 
+
+const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem("state")
+        if (serializedState === null) return undefined
+        return JSON.parse(serializedState)
+    } catch(e) {
+        console.log(e)
+        return undefined
+    }
+}
+
+const persistedState = loadFromLocalStorage()
+
 const store = createStore(
     allReducers,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
     )
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 ReactDOM.render(
     <Provider store={store}>
